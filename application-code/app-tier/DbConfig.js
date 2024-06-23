5ef5e3102309f6b611cbc2c6d09ec4555ad1e153
shivgopal <39775114+shivgopal@users.noreply.github.com>
2024-06-23 15:08:53 +0530

    Update DbConfig.js new

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1-instance-1.c16mku6i4nlu.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'shiv1234',
+    DB_DATABASE : 'webappdb'
 });
